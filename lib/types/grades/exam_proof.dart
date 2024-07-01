25dee489b5eb6e7f1edd9318d0b061d00b3726ed
oscarspalk <oscar.gaardsted.spalk@gmail.com>
2024-07-02 01:16:05 +0200

    average calculator

--- lib/types/grades/exam_proof.dart
+++ lib/types/grades/exam_proof.dart
@@ -6,6 +6,20 @@ class ExamProof {
   ExamProof({required this.entries});
 
   double calculateAverage() {
-    return 0.0;
+    double sum = 0.0;
+    double weights = 0.0;
+    for (var entry in entries) {
+      if (entry.examGrade != null) {
+        var grade = entry.examGrade!.grade;
+        sum += grade.weight * grade.grade;
+        weights += grade.weight;
+      }
+      if (entry.yearGrade != null) {
+        var grade = entry.yearGrade!.grade;
+        sum += grade.weight * grade.grade;
+        weights += grade.weight;
+      }
+    }
+    return sum / weights;
   }
 }
